a38446832f61bc55fa47105eae560f5e2d84c2b5
CR <44606665+CodebyCR@users.noreply.github.com>
2022-07-26 21:47:20 +0200

    Adds some GUI components for settings which follows in the future.

--- src/Settings.py
+++ src/Settings.py
@@ -14,6 +14,14 @@ SECONDARY = cs.SECONDARY
 FONT_1 = cs.FONT_1
 FONT_2 = cs.FONT_2
 
+def set_fill_color(color):
+    cs.FILL_COLOR = color
+
+def set_background_color(color):
+    cs.BACK_COLOR = color
+
+
+
 def switch_language(lang):
     print(lang)
     pass
@@ -25,6 +33,7 @@ def get_settings():
     settings.title('Settings')
     settings.geometry("600x330")
 
+    # Language
     langLabel = Label(settings, text="Language:")
     langLabel.configure(background=BACKGROUND, font=FONT_1, padding=10)
     langLabel.grid(column=0, row=2)
@@ -39,7 +48,71 @@ def get_settings():
     chooseLang.grid(column=1, row=2)
     chooseLang.current(1)
 
+    # Error Level
+    errorLabel = Label(settings, text="QR-Code Error Level:")
+    errorLabel.configure(background=BACKGROUND, font=FONT_1, padding=10)
+    errorLabel.grid(column=0, row=3)
+
+    chooseError = Combobox(settings, state="readonly",
+                                values=["L",
+                                        "M",
+                                        "Q",
+                                        "H"]
+                                )
+    chooseError.configure(background=BACKGROUND, font=FONT_1,
+                            takefocus=NONE, justify=CENTER)
+    chooseError.grid(column=1, row=3)
+    chooseError.current(1)
+
+    # QR Code Color Scheme
+    color_label = Label(settings, text="QR-Code Color Scheme:")
+    color_label.configure(background=BACKGROUND, font=FONT_1, padding=10)
+    color_label.grid(column=1, row=4, columnspan=2)
+
+
+    inColorLabel = Label(settings, text="Fill Color:")
+    inColorLabel.configure(background=BACKGROUND, font=FONT_1, padding=10)
+    inColorLabel.grid(column=0, row=5)
+
+    chooseInColor = Combobox(settings, state="readonly",
+                                values=["Black",
+                                        "White"]
+                                )
+    chooseInColor.configure(background=BACKGROUND, font=FONT_1,
+                            takefocus=NONE, justify=CENTER)
+    chooseInColor.grid(column=1, row=5)
+    chooseInColor.current(1)
+
+    outColorLabel = Label(settings, text="Background Color:")
+    outColorLabel.configure(background=BACKGROUND, font=FONT_1, padding=10)
+    outColorLabel.grid(column=0, row=6)
+
+    chooseOutColor = Combobox(settings, state="readonly",
+                                values=["Black",
+                                        "White"]
+                                )
+    chooseOutColor.configure(background=BACKGROUND, font=FONT_1,
+                            takefocus=NONE, justify=CENTER)
+    chooseOutColor.grid(column=1, row=6)
+    chooseOutColor.current(1)
+
+    # QR Code Size
+    sizeLabel = Label(settings, text="QR-Code Size:")
+    sizeLabel.configure(background=BACKGROUND, font=FONT_1, padding=10)
+    sizeLabel.grid(column=0, row=7)
+
+    chooseSize = Combobox(settings, state="readonly",
+                                values=["Small",
+                                        "Medium",
+                                        "Large"]
+                                )
+    chooseSize.configure(background=BACKGROUND, font=FONT_1,
+                            takefocus=NONE, justify=CENTER)
+    chooseSize.grid(column=1, row=7)
+    chooseSize.current(1)
+
+
     apply_button = Button(settings, text="Apply", command=settings.destroy)
-    apply_button.grid(column=1, row=3, columnspan=2, padx=10, pady=100)
+    apply_button.grid(column=2, row=8, columnspan=2, padx=10, pady=10)
     # pprint(dict(chooseLang))
     settings.mainloop()
